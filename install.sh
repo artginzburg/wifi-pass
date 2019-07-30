d9ae2772fe78cdb2a3cb3839ab8ee9e403adeae2
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-07-30 05:24:25 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-curl https://raw.github.com/DaFuqtor/wifi-pass/master/wifi-pass.sh -o /usr/local/bin/wifi-pass && chmod +x /usr/local/bin/wifi-pass
+curl https://raw.githubusercontent.com/DaFuqtor/wifi-pass/master/wifi-pass.sh -o /usr/local/bin/wifi-pass && chmod +x /usr/local/bin/wifi-pass
